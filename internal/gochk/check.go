99eedfd78c0d560d2599d56cabab784ec602bb3b
resotto <resotto3@gmail.com>
2020-10-08 10:02:59 +0900

    fix print layout

--- internal/gochk/check.go
+++ internal/gochk/check.go
@@ -16,12 +16,15 @@ const (
 )
 
 type dependency struct {
-	path  string
-	index int
+	filepath     string
+	currentLayer int
+	path         string
+	index        int
 }
 
 // Check makes sure the direction of dependency is correct
 func Check(cfg Config) {
+	errorDeps := make([]dependency, 0, 0)
 	err := filepath.Walk(cfg.TargetPath, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Println(err)
@@ -38,21 +41,28 @@ func Check(cfg Config) {
 		if info.IsDir() || !strings.Contains(info.Name(), ".go") {
 			return nil
 		}
-		checkDependency(cfg.DependencyOrders, path)
+		tempDeps := checkDependency(cfg.DependencyOrders, path)
+		errorDeps = append(errorDeps, tempDeps...)
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
+	if len(errorDeps) > 0 {
+		for _, d := range errorDeps {
+			print(red, "[Error]    "+d.filepath+" imports "+d.path)
+			print(red, "           \""+cfg.DependencyOrders[d.currentLayer]+"\" depends on \""+cfg.DependencyOrders[d.index]+"\"")
+		}
+	}
 }
 
-func checkDependency(dependencies []string, path string) {
+func checkDependency(dependencies []string, path string) []dependency {
 	currentLayer := search(dependencies, path)
-	importLayers := retrieveLayers(dependencies, path)
+	importLayers := retrieveLayers(dependencies, path, currentLayer)
 
 	if len(importLayers) == 0 {
 		print(teal, "[None]     "+path)
-		return
+		return nil
 	}
 	redDeps := make([]dependency, 0, len(importLayers))
 
@@ -63,16 +73,13 @@ func checkDependency(dependencies []string, path string) {
 		}
 	}
 	if len(redDeps) > 0 {
-		for _, d := range redDeps {
-			print(red, "[Error]    "+path+" imports "+d.path)
-			print(red, "           \""+dependencies[currentLayer]+"\" depends on \""+dependencies[d.index]+"\"")
-		}
-	} else {
-		print(green, "[Verified] "+path)
+		return redDeps
 	}
+	print(green, "[Verified] "+path)
+	return nil
 }
 
-func retrieveLayers(dependencies []string, path string) []dependency {
+func retrieveLayers(dependencies []string, path string, currentLayer int) []dependency {
 	filepath, _ := filepath.Abs(path)
 	imports := readImports(filepath)
 	layers := make([]dependency, 0, len(imports))
@@ -81,8 +88,10 @@ func retrieveLayers(dependencies []string, path string) []dependency {
 		l := search(dependencies, v)
 		if l != -1 {
 			layers = append(layers, dependency{
-				path:  v,
-				index: l,
+				filepath:     path,
+				currentLayer: currentLayer,
+				path:         v,
+				index:        l,
 			})
 		}
 	}
