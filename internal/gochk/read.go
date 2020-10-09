0fd8e3929c63e2920a5898287a895f7463b0b6b9
resotto <resotto3@gmail.com>
2020-10-09 09:51:56 +0900

    refactor read.go

--- internal/gochk/read.go
+++ internal/gochk/read.go
@@ -10,31 +10,33 @@ func readImports(filepath string) []string {
 	f, _ := os.Open(filepath)
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
-	multipleImport := false
-	imports := make([]string, 0, 10)
 	for scanner.Scan() {
 		line := scanner.Text()
 		if len(line) > 6 && strings.EqualFold(line[:6], "import") {
 			if strings.Contains(line, "(") {
-				multipleImport = true
-				continue
+				return retrieveMultipleImportPath(scanner, line)
 			}
-			imports = append(imports, retrievePath(line))
+			return []string{retrieveImportPath(line)}
+		}
+	}
+	return []string{}
+}
+
+func retrieveMultipleImportPath(scanner *bufio.Scanner, line string) []string {
+	imports := make([]string, 0, 10)
+	for scanner.Scan() {
+		line = scanner.Text()
+		if strings.Contains(line, ")") {
 			break
+		} else if strings.EqualFold(line, "") {
+			continue
 		}
-		if multipleImport {
-			if strings.Contains(line, ")") {
-				break
-			} else if strings.EqualFold(line, "") {
-				continue
-			}
-			imports = append(imports, retrievePath(line))
-		}
+		imports = append(imports, retrieveImportPath(line))
 	}
 	return imports
 }
 
-func retrievePath(line string) string {
+func retrieveImportPath(line string) string {
 	firstQuoIndex := strings.Index(line, "\"")
 	return line[firstQuoIndex:]
 }
