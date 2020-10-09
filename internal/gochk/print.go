68ec94ae1b2bed3a0b35befdb2c897bd74e6ec52
resotto <resotto3@gmail.com>
2020-10-09 21:16:05 +0900

    refactor print.go

--- internal/gochk/print.go
+++ internal/gochk/print.go
@@ -3,30 +3,32 @@ package gochk
 import "fmt"
 
 const (
-	red    = "\033[1;31m%s\033[0m"
-	yellow = "\033[1;33m%s\033[0m"
-	green  = "\033[1;32m%s\033[0m"
-	teal   = "\033[1;36m%s\033[0m"
+	red    = "\033[1;31m%-11s%s\n\033[0m"
+	yellow = "\033[1;33m%-11s%s\n\033[0m"
+	green  = "\033[1;32m%-11s%s\n\033[0m"
+	teal   = "\033[1;36m%-11s%s\n\033[0m"
 )
 
-func print(color string, message string) {
-	fmt.Printf(color, message)
-	fmt.Println()
-}
+const (
+	none     = "[None]"
+	verified = "[Verified]"
+	ignored  = "[Ignored]"
+	err      = "[Error]"
+)
 
 func printNone(path string) {
-	print(teal, "[None]     "+path)
+	fmt.Printf(teal, none, path)
 }
 
 func printVerified(path string) {
-	print(green, "[Verified] "+path)
+	fmt.Printf(green, verified, path)
 }
 
 func printIgnored(path string) {
-	print(yellow, "[Ignored]  "+path)
+	fmt.Printf(yellow, ignored, path)
 }
 
 func printError(filepath string, path string, dependencyOrders []string, currentLayer int, index int) {
-	print(red, "[Error]    "+filepath+" imports "+path)
-	print(red, "           \""+dependencyOrders[currentLayer]+"\" depends on \""+dependencyOrders[index]+"\"")
+	fmt.Printf(red, err, filepath+" imports "+path)
+	fmt.Printf(red, "", dependencyOrders[currentLayer]+" depends on "+dependencyOrders[index])
 }
