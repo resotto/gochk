d7dc9980e101899b5dfc65ed62b0bf110de6b10e
resotto <resotto3@gmail.com>
2020-10-10 13:28:42 +0900

    skip block comments

--- internal/gochk/read.go
+++ internal/gochk/read.go
@@ -8,15 +8,38 @@ import (
 
 func readImports(f *os.File) []string {
 	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		if line := scanner.Text(); len(line) > 6 && strings.EqualFold(line[:6], "import") {
-			if strings.Contains(line, "(") {
-				return retrieveMultipleImportPath(scanner, line)
-			}
-			return []string{retrieveImportPath(line)}
+	skipToImportStatement(scanner)
+	scanner.Scan()
+	if line := scanner.Text(); len(line) > 6 && strings.EqualFold(line[:6], "import") {
+		if strings.Contains(line, "(") {
+			return retrieveMultipleImportPath(scanner, line)
 		}
+		return []string{retrieveImportPath(line)}
 	}
 	return []string{}
+}
+
+func skipToImportStatement(scanner *bufio.Scanner) {
+	scanner.Scan()
+	line := scanner.Text()
+	skipBlockComments(line, scanner)
+	for true {
+		if line := scanner.Text(); len(line) > 7 && strings.EqualFold(line[:7], "package") {
+			scanner.Scan() // Points to two lines below the "package" declaration
+			return
+		}
+		scanner.Scan()
+	}
+}
+
+func skipBlockComments(line string, scanner *bufio.Scanner) {
+	if strings.EqualFold(line, "/*") {
+		for scanner.Scan() {
+			if line := scanner.Text(); strings.EqualFold(line, "*/") {
+				return
+			}
+		}
+	}
 }
 
 func retrieveMultipleImportPath(scanner *bufio.Scanner, line string) []string {
